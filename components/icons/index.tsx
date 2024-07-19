f175776bae0523eae4af610d24b7d4eb06d7b04c
Kihyun Ryu <khryu0610@gmail.com>
2024-07-19 21:00:31 +0900

    Feature: add UIs

---
+++ components/icons/index.tsx
@@ -0,0 +1,9 @@
+export function Icons() {
+  return <div></div>;
+}
+
+function logo(props: any) {
+  return <p {...props}>vottega&apos;s logo</p>;
+}
+
+Icons.logo = logo;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const phoneRegex = new RegExp(/^(01[016789]{1})[0-9]{3,4}[0-9]{4}$/);
+
+export const formatPhone = (phone: string): string => {
+  if (!phoneRegex.test(phone)) {
+    throw new Error("올바른 전화번호 형식이 아닙니다.");
+  }
+
+  const breakpoint = phone.length === 11 ? 7 : 6;
+  return `${phone.slice(0, 3)}-${phone.slice(3, breakpoint)}-${phone.slice(breakpoint)}`;
+};
