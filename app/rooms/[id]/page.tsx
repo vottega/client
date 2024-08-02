aa38722f16f4126343e3621a80d004a049ea8d76
Kihyun Ryu <khryu0610@gmail.com>
2024-08-02 22:57:29 +0900

    Feature: Invitation Table, Form UI & Features
    * features: notification, add, delete

--- app/rooms/[id]/page.tsx
+++ app/rooms/[id]/page.tsx
@@ -13,6 +13,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -26,11 +27,11 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { toast } from "@/components/ui/use-toast";
 import { formatPhone, phoneRegex } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Award, X } from "lucide-react";
-import { MouseEvent, useMemo, useState } from "react";
+import { CheckedState } from "@radix-ui/react-checkbox";
+import { Award, CircleCheck, CircleX, Trash2 } from "lucide-react";
+import { MouseEvent, useEffect, useMemo, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -44,7 +45,7 @@ const FormSchema = z.object({
     .string({
       required_error: "번호를 입력해주세요.",
     })
-    .regex(phoneRegex, { message: "유효하지 않은 전화번호입니다." }),
+    .regex(phoneRegex, { message: "유효하지 않은 전화번호 형식입니다. (예시: 01012345678)" }),
 });
 
 type Invitation = z.infer<typeof FormSchema>;
@@ -69,17 +70,18 @@ export function InvitationForm({
     },
   });
 
+  const [notification, setNotification] = useState<"idle" | "success" | "fail">("idle");
+  const [animate, setAnimate] = useState<boolean>(true);
+
   function onSubmit(data: z.infer<typeof FormSchema>) {
     if (invitations.find(({ phone }) => phone === data.phone)) {
-      toast({
-        title: "올바르지 않은 전화번호",
-        description: "이미 등록된 전화번호예요. 전화번호를 확인해주세요.",
-      });
-
-      return;
+      setNotification("fail");
+    } else {
+      setInvitations((prev: Invitation[]) => [...prev, data]);
+      setNotification("success");
     }
 
-    setInvitations((prev: Invitation[]) => [...prev, data]);
+    setAnimate(true);
   }
 
   return (
@@ -106,7 +108,7 @@ export function InvitationForm({
             <FormItem className="flex flex-col">
               <FormLabel>전화번호</FormLabel>
               <FormControl>
-                <Input {...field} placeholder="전화번호를 입력해주세요." />
+                <Input type="tel" {...field} placeholder="전화번호를 입력해주세요." />
               </FormControl>
               <FormDescription>
                 전화번호가 없으면 추후 다른 방법으로 초대할 수 있어요.
@@ -115,7 +117,29 @@ export function InvitationForm({
             </FormItem>
           )}
         />
-        <Button type="submit">추가하기</Button>
+        <div className="flex gap-4 items-center">
+          <Button type="submit">추가하기</Button>
+          {notification === "success" && (
+            <CircleCheck
+              color="green"
+              strokeDasharray={200}
+              className="[stroke-dashoffset:250] fill-mode-forwards animate-draw"
+            />
+          )}
+          {notification === "fail" && (
+            <CircleX
+              color="hsl(var(--destructive))"
+              className={`fill-mode-forwards ${animate && "animate-bounce-horizontal"}`}
+              onAnimationEnd={() => setAnimate(false)}
+            />
+          )}
+        </div>
+
+        <p
+          className={`text-sm font-medium text-destructive transition-opacity ${notification === "fail" ? "visible opacity-100" : "invisible opacity-0"}`}
+        >
+          이미 등록된 전화번호예요. 전화번호를 확인해주세요.
+        </p>
       </form>
     </Form>
   );
@@ -129,94 +153,160 @@ export function InvitationTable({
   setInvitations: any;
 }) {
   const [me, ...others] = useMemo(() => invitations, [invitations]);
-
-  const onClickDeleteButton = (e: MouseEvent<HTMLButtonElement>, index: number) => {
-    if (index < 0 || index >= invitations.length) {
-      return;
+  const prevInvitationsLength = useRef<number>(1);
+  const [selected, setSelected] = useState<Set<number>>(new Set());
+  const selectedCount = useMemo(() => selected.size, [selected]);
+  const SKELETON_FILL = 3;
+  const tBody = useRef<HTMLTableSectionElement>(null);
+  console.log("render");
+  useEffect(() => {
+    if (invitations.length > prevInvitationsLength.current) {
+      tBody.current?.lastElementChild?.scrollIntoView({ behavior: "smooth" });
     }
-    setInvitations([...invitations.slice(0, index), ...invitations.slice(index + 1)]);
+    prevInvitationsLength.current = invitations.length;
+  }, [invitations]);
+
+  const deselectAll = () => {
+    setSelected((prev) => new Set());
+  };
+  const onClickDeleteButton = (e: MouseEvent<HTMLButtonElement>) => {
+    setInvitations((invitations: Invitation[]) =>
+      invitations.filter((_, idx) => !selected.has(idx)),
+    );
+
+    deselectAll();
+  };
+  const onCheckedChange = (checked: CheckedState, idx: number) => {
+    if (checked) {
+      setSelected((prev) => {
+        const newSet = new Set(prev);
+        newSet.add(idx);
+        return newSet;
+      });
+    } else {
+      setSelected((prev) => {
+        const newSet = new Set(prev);
+        newSet.delete(idx);
+        return newSet;
+      });
+    }
   };
 
-  function TableRowInvitation({ invitation, idx }: { invitation: Invitation; idx: number }) {
-    const [isHover, setIsHover] = useState<boolean>();
-    const toggleHover = () => {
-      setIsHover((prev) => !prev);
-    };
-    console.log("rendering: ", idx);
-
-    return (
-      <TableRow onMouseOver={toggleHover} onMouseOut={toggleHover} className="relative">
-        <TableCell className="font-medium">
-          <span className="animate-fadein">{invitation.name}</span>
-        </TableCell>
-        <TableCell className={`text-right transition-transform ${isHover && "-translate-x-4"}`}>
-          <span className="animate-fadein">{formatPhone(invitation.phone)}</span>
-        </TableCell>
-        {isHover && (
-          <Button
-            className="absolute right-0 bottom-[50%] translate-y-1/2"
-            size={"icon-sm"}
-            onClick={(e) => {
-              onClickDeleteButton(e, idx);
-            }}
-          >
-            <X size={16} />
-          </Button>
-        )}
-      </TableRow>
-    );
-  }
-
   return (
-    <Table>
-      <TableCaption>현재까지 초대된 목록이에요.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="">성함</TableHead>
-          <TableHead className="text-right">전화번호</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">
-            {me.name}
-            <Badge className="align-bottom">
-              <Award size={14} />
-            </Badge>
-          </TableCell>
-          <TableCell className="text-right">{formatPhone(me.phone)}</TableCell>
-        </TableRow>
+    <>
+      <div className="flex justify-between items-center pl-4 border-b">
+        <span className="text-sm">{selectedCount} 개 선택됨</span>
+        <Button
+          variant={"ghost"}
+          size={"sm"}
+          className="text-destructive hover:text-destructive"
+          onClick={onClickDeleteButton}
+        >
+          삭제하기
+          <Trash2 size={16} color="hsl(var(--destructive))" />
+        </Button>
+      </div>
 
-        {[...Array(2)].map((_, idx) => (
-          <TableRow key={idx}>
-            <TableCell className="font-medium">
-              {others[idx] ? (
-                <span className="animate-fadein">{others[idx].name}</span>
-              ) : (
-                <Skeleton className="h-6 w-10" />
-              )}
-            </TableCell>
-            <TableCell className="text-right">
-              {others[idx] ? (
-                <span className="animate-fadein">{formatPhone(others[idx].phone)}</span>
-              ) : (
-                <Skeleton className="h-6 w-28 ml-auto" />
-              )}
-            </TableCell>
+      <Table>
+        <TableCaption className="sticky bottom-0 bg-white mt-0 pt-4 z-10">
+          현재까지 초대된 목록이에요.
+        </TableCaption>
+        <TableHeader className="sticky top-0 bg-white z-10">
+          <TableRow>
+            <TableHead>
+              <Checkbox className="align-text-bottom" aria-label="전체 행 선택" />
+            </TableHead>
+            <TableHead>성함</TableHead>
+            <TableHead className="text-right">전화번호</TableHead>
           </TableRow>
-        ))}
+        </TableHeader>
 
-        {others.slice(2).map((invitation, idx) => (
-          <TableRowInvitation key={invitation.phone} invitation={invitation} idx={idx} />
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={1}>총 인원 수</TableCell>
-          <TableCell className="text-right">{invitations.length}명</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        <TableBody ref={tBody}>
+          <TableRow className="h-[60px] relative">
+            <TableCell></TableCell>
+            <TableCell className="font-medium">
+              {me.name}
+              <Badge className="align-bottom">
+                <Award size={14} />
+              </Badge>
+            </TableCell>
+            <TableCell className="text-right">{formatPhone(me.phone)}</TableCell>
+          </TableRow>
+
+          {[...Array(SKELETON_FILL)].map((_, idx) => (
+            <TableRow key={idx + 1} className="relative h-[60px] z-0">
+              <TableCell>
+                {others[idx] && (
+                  <>
+                    <Checkbox
+                      className="align-text-bottom animate-fadein"
+                      aria-label="행 선택"
+                      onCheckedChange={(checked) => {
+                        onCheckedChange(checked, idx + 1);
+                      }}
+                      id={others[idx].phone}
+                    />
+                    <Label
+                      htmlFor={others[idx].phone}
+                      className="block w-full h-[60px] absolute bottom-0 left-0 cursor-pointer"
+                    ></Label>
+                  </>
+                )}
+              </TableCell>
+              <TableCell>
+                {others[idx] ? (
+                  <span className="animate-fadein">{others[idx].name}</span>
+                ) : (
+                  <Skeleton className="h-6 w-10" />
+                )}
+              </TableCell>
+              <TableCell className="text-right">
+                {others[idx] ? (
+                  <span className="animate-fadein">{formatPhone(others[idx].phone)}</span>
+                ) : (
+                  <Skeleton className="h-6 w-28 ml-auto" />
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+
+          {others.slice(SKELETON_FILL).map((invitation, idx) => (
+            <TableRow key={invitation.phone} className="relative h-[60px] z-0">
+              <TableCell>
+                <Checkbox
+                  className="align-text-bottom"
+                  aria-label="행 선택"
+                  onCheckedChange={(checked) => {
+                    onCheckedChange(checked, idx + 1 + SKELETON_FILL);
+                  }}
+                  id={invitations[idx + 1 + SKELETON_FILL].phone}
+                />
+                <Label
+                  htmlFor={invitations[idx + 1 + SKELETON_FILL].phone}
+                  className="block w-full h-[60px] absolute bottom-0 left-0 cursor-pointer"
+                ></Label>
+              </TableCell>
+              <TableCell className="font-medium">
+                <span className="animate-fadein">{invitation.name}</span>
+              </TableCell>
+              <TableCell className="text-right">
+                <span className="animate-fadein">{formatPhone(invitation.phone)}</span>
+              </TableCell>
+            </TableRow>
+          ))}
+
+          <TableRow className="sr-only">
+            <TableCell></TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter className="sticky bottom-[36px] bg-muted">
+          <TableRow>
+            <TableCell colSpan={2}>총 인원 수</TableCell>
+            <TableCell className="text-right">{invitations.length}명</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </>
   );
 }
 
@@ -245,10 +335,15 @@ export default function RoomInit() {
   ]);
 
   return (
-    <div className="py-20 px-4 flex flex-col items-center h-screen gap-10">
-      <header className="w-full flex flex-col items-center gap-4">
-        <p className="text-center">{`${step} / ${Object.keys(steps).length}`}</p>
-        <Progress value={(step / Object.keys(steps).length) * 100} className="w-[60%]" />
+    <div className="py-10 px-4 flex flex-col items-center h-screen gap-10">
+      <header className="w-full flex flex-col items-center gap-2">
+        <h1>방 이름</h1>
+        <p className="text-center">{`${step} / ${Object.keys(steps).length}`} 초대 전송하기</p>
+        <Progress
+          value={(step / Object.keys(steps).length) * 100}
+          className="w-[60%]"
+          aria-label="방 만들기 진행 과정"
+        />
       </header>
 
       {/* step 1 */}
@@ -266,21 +361,21 @@ export default function RoomInit() {
               <InvitationForm invitations={invitations} setInvitations={setInvitations} />
             </div>
             <Separator orientation="vertical" />
-            <div className="flex-1">
+            <div className="flex-1 flex flex-col">
               <InvitationTable invitations={invitations} setInvitations={setInvitations} />
             </div>
           </div>
         </>
       )}
 
-      <Button
+      {/* <Button
         className="w-full py-6"
         onClick={() =>
           setStep((prev): Step => (prev === 3 ? (prev as Step) : ((prev + 1) as Step)))
         }
       >
         다음 단계로
-      </Button>
+      </Button> */}
     </div>
   );
 }
