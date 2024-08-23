d3a41dede6a7e3c6e9612fcc0c3481f430d75a22
Kihyun Ryu <khryu0610@gmail.com>
2024-08-23 17:11:54 +0900

    Fix: Combobox 유연하게 변경

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check, ChevronsUpDown, Plus } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -15,32 +15,21 @@ import {
 } from "@/components/ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
+export type Command = {
+  value: string;
+  label: string;
+};
 
-export function Combobox() {
+export function Combobox({
+  commands,
+  defaultValue,
+}: {
+  commands: Command[];
+  defaultValue?: string;
+}) {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const [value, setValue] = React.useState(defaultValue ?? "");
+  const [newRole, setNewRole] = React.useState("");
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -49,24 +38,29 @@ export function Combobox() {
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-[120px] justify-between"
         >
-          {value
-            ? frameworks.find((framework) => framework.value === value)?.label
-            : "Select framework..."}
+          {value ? commands.find((command) => command.value === value)?.label : "역할 선택"}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-[120px] p-0">
         <Command>
-          <CommandInput placeholder="Search framework..." />
-          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandInput placeholder="역할 검색" onValueChange={(search) => setNewRole(search)} />
+          <CommandEmpty className="p-0">
+            <div className="overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground">
+              <div className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+                <Plus className={"mr-2 h-4 w-4 "} />
+                {newRole}
+              </div>
+            </div>
+          </CommandEmpty>
           <CommandList>
             <CommandGroup>
-              {frameworks.map((framework) => (
+              {commands.map((command) => (
                 <CommandItem
-                  key={framework.value}
-                  value={framework.value}
+                  key={command.value}
+                  value={command.value}
                   onSelect={(currentValue) => {
                     setValue(currentValue === value ? "" : currentValue);
                     setOpen(false);
@@ -75,10 +69,10 @@ export function Combobox() {
                   <Check
                     className={cn(
                       "mr-2 h-4 w-4",
-                      value === framework.value ? "opacity-100" : "opacity-0",
+                      value === command.value ? "opacity-100" : "opacity-0",
                     )}
                   />
-                  {framework.label}
+                  {command.label}
                 </CommandItem>
               ))}
             </CommandGroup>
