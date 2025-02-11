aa9e946e48c7655d185e8eba60dc014c7c132d25
Kihyun Ryu <khryu0610@gmail.com>
2025-02-11 23:29:46 +0900

    Feature: 새로운 참가자 역할 UI 작성

--- app/rooms/new/page.tsx
+++ app/rooms/new/page.tsx
@@ -3,14 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Data } from "@/components/ui/combobox";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
 import { DialogFooter } from "@/components/ui/dialog";
 import {
   Form,
@@ -23,14 +15,21 @@ import {
 import { TheHeader } from "@/components/ui/header";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Switch } from "@/components/ui/switch";
 import { ROLES, Roles } from "@/constants/role";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Plus, Shield, X } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { Dispatch, forwardRef, HTMLAttributes, SetStateAction, useMemo, useState } from "react";
+import {
+  Dispatch,
+  forwardRef,
+  HTMLAttributes,
+  KeyboardEvent,
+  SetStateAction,
+  useMemo,
+  useState,
+} from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -116,19 +115,7 @@ export default function Page() {
                                 {role}
                               </RoleBadgeWithCanVoteSwitch>
                             ))}
-                            <Combobox
-                              datas={datas}
-                              onRoleAdded={(role: string) => {
-                                setRoles((prev) => {
-                                  if (prev.has(role)) {
-                                    return prev;
-                                  }
-                                  const nextRoles = new Map(prev);
-                                  nextRoles.set(role, { value: role, canVote: true });
-                                  return nextRoles;
-                                });
-                              }}
-                            ></Combobox>
+                            <AddRoleBadge setRoles={setRoles} />
                           </div>
                         </CardContent>
                       </Card>
@@ -217,22 +204,24 @@ const RoleBadgeWithCanVoteSwitch = ({
   };
 
   return (
-    <RoleBadge className="p-4 rounded-lg relative" variant={variant}>
-      {isDefaultRole ? (
-        <Shield size={16} color="hsl(var(--primary))" />
-      ) : (
-        <Button
-          type="button"
-          onClick={handleDeleteButton}
-          variant="ghost"
-          size="icon-sm"
-          className="w-fit h-fit"
-        >
-          <X size={16} color="hsl(var(--muted-foreground))" />
-        </Button>
-      )}
-      <span className="font-medium">{children}</span>
-      <div className="absolute right-3 flex items-center gap-2">
+    <RoleBadge className="h-[54px] px-4 rounded-lg relative justify-between" variant={variant}>
+      <div className="flex items-center gap-2">
+        {isDefaultRole ? (
+          <Shield size={16} color="hsl(var(--primary))" />
+        ) : (
+          <Button
+            type="button"
+            onClick={handleDeleteButton}
+            variant="ghost"
+            size="icon-sm"
+            className="w-fit h-fit"
+          >
+            <X size={16} color="hsl(var(--muted-foreground))" />
+          </Button>
+        )}
+        <span className="font-medium break-all text-overflow">{children}</span>
+      </div>
+      <div className="flex items-center gap-2 shrink-0">
         <Label htmlFor={`${children}-can-vote`} className="text-muted-foreground">
           투표권
         </Label>
@@ -247,77 +236,50 @@ const RoleBadgeWithCanVoteSwitch = ({
   );
 };
 
-const AddRoleBadge = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  ({ ...props }, ref) => {
-    return (
-      <BaseBadge
-        className="hover:bg-accent hover:text-accent-foreground p-4 rounded-lg cursor-pointer border-dashed"
-        ref={ref}
-        {...props}
-      >
-        <Plus size={16} />
-        역할 추가
-      </BaseBadge>
-    );
-  },
-);
-AddRoleBadge.displayName = "AddRoleBadge";
-
-export function Combobox({ datas, onRoleAdded }: { datas: Data[]; onRoleAdded: any }) {
-  const [open, setOpen] = useState(false);
+const AddRoleBadge = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement> & { setRoles: Dispatch<SetStateAction<Roles>> }
+>(({ setRoles, ...props }, ref) => {
+  const [showInput, setShowInput] = useState(false);
   const [newRole, setNewRole] = useState("");
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter" && newRole !== "") {
+      setShowInput(false);
+      setRoles((prev) => {
+        const nextRoles = new Map(prev);
+        if (nextRoles.has(newRole)) return nextRoles;
+        nextRoles.set(newRole, { value: newRole, canVote: true });
+        return nextRoles;
+      });
+      setNewRole("");
+    }
+
+    if (e.key === "Escape") {
+      setShowInput(false);
+    }
+  };
 
   return (
-    <Popover
-      open={open}
-      onOpenChange={(open) => {
-        setOpen(open);
-        setNewRole("");
-      }}
+    <BaseBadge
+      className="hover:bg-accent hover:text-accent-foreground h-[54px] px-4 rounded-lg cursor-pointer border-dashed"
+      ref={ref}
+      {...props}
+      onClick={() => setShowInput(true)}
     >
-      <PopoverTrigger asChild>
-        <AddRoleBadge role="combobox" aria-expanded={open} />
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput
-            placeholder="역할 검색"
-            onValueChange={(search) => setNewRole(search)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && newRole !== "") {
-                setOpen(false);
-                onRoleAdded(newRole);
-              }
-            }}
-          />
-          <CommandEmpty className="p-0">
-            {(newRole !== "" || datas.length !== 0) && (
-              <div className="overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground">
-                <div className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
-                  <Plus className={"mr-2 h-4 w-4 "} />
-                  <RoleBadge variant={0}>{newRole}</RoleBadge>
-                </div>
-              </div>
-            )}
-          </CommandEmpty>
-          <CommandList>
-            <CommandGroup heading="추천">
-              {datas.map((data, idx) => (
-                <CommandItem
-                  key={data.value}
-                  value={data.value}
-                  onSelect={(currentValue) => {
-                    setOpen(false);
-                    onRoleAdded(currentValue);
-                  }}
-                >
-                  <RoleBadge variant={idx + 3}>{data.label}</RoleBadge>
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+      <Plus size={16} />
+      {showInput ? (
+        <Input
+          className="w-[200px] h-[30px]"
+          placeholder="예: 부의장, 보조 서기, 타임키퍼"
+          onChange={(e) => setNewRole(e.target.value)}
+          value={newRole}
+          onKeyDown={handleKeyDown}
+          autoFocus
+        />
+      ) : (
+        "역할 추가"
+      )}
+    </BaseBadge>
   );
-}
+});
+AddRoleBadge.displayName = "AddRoleBadge";
