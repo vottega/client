b423b1a283c87e93c0a24a280e5e4de101a76299
Kihyun Ryu <khryu0610@gmail.com>
2025-03-19 20:43:46 +0900

    Feature: 회의실 생성 백엔드 연결

--- app/rooms/new/page.tsx
+++ app/rooms/new/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { APIErrorResponse } from "@/app/api/types";
+import { CreateRoomRequest, CreateRoomResponse } from "@/app/api/types/room";
 import { RoleList } from "@/app/rooms/RoleList";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -15,11 +17,12 @@ import {
 } from "@/components/ui/form";
 import { TheHeader } from "@/components/ui/header";
 import { Input } from "@/components/ui/input";
-import { ROLES, Roles } from "@/constants/role";
+import { Roles, ROLES } from "@/constants/role";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import useSWRMutation from "swr/mutation";
 import { z } from "zod";
 
 export default function Page() {
@@ -28,7 +31,7 @@ export default function Page() {
     roomName: z.string().min(1, { message: "회의실 이름을 입력해주세요." }),
     participantRoleList: z.array(
       z.object({
-        value: z.string(),
+        role: z.string(),
         canVote: z.boolean(),
       }),
     ),
@@ -42,10 +45,47 @@ export default function Page() {
   });
   const [roles, setRoles] = useState<Roles>(ROLES);
 
+  const createRoom = async (url: string, { arg }: { arg: CreateRoomRequest }) => {
+    const headers = new Headers();
+    headers.set("Content-Type", "application/json");
+
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(arg),
+      headers,
+    });
+
+    const data: CreateRoomResponse | APIErrorResponse = await response.json();
+
+    if (!response.ok) {
+      // TODO: 에러처리 및 에러UI
+      return null;
+    }
+
+    return data as CreateRoomResponse;
+  };
+
+  // TODO: 에러/로딩 처리, 미들웨어 작성
+  const { trigger, data, error } = useSWRMutation<
+    CreateRoomResponse | null,
+    any,
+    string,
+    CreateRoomRequest,
+    CreateRoomResponse
+  >("http://localhost:8082/api/room", createRoom);
+
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    const roomId = 1;
-    router.push(`/rooms/${roomId}`);
+    const newData: typeof data = { ...data, participantRoleList: [...roles.values()] };
+    const createRoomRequestBody = { ...newData, ownerId: 1 };
+    trigger(createRoomRequestBody);
   }
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+      router.push(`/rooms/${data.id}`);
+    }
+  }, [data, router]);
 
   return (
     <>
@@ -85,7 +125,7 @@ export default function Page() {
                     <FormControl>
                       <Card>
                         <CardContent className="p-4">
-                          <RoleList />
+                          <RoleList roles={roles} setRoles={setRoles} />
                         </CardContent>
                       </Card>
                     </FormControl>
