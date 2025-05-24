4a27969b2a1261b7447f7da63d1cf4351d4b0976
Kihyun Ryu <khryu0610@medicalai.com>
2025-05-24 18:52:51 +0900

    Chore: auth DTO 작성

---
+++ lib/api/types/auth-service.dto.ts
@@ -0,0 +1,43 @@
+import { UUID } from "crypto";
+
+export type Role = "USER" | "PARTICIPANT";
+
+export interface ParticipantAuthRequestDTO {
+  participantId: UUID;
+}
+
+export interface UserAuthRequestDTO {
+  id: number;
+  userId: string;
+}
+
+export interface AuthResponseDTO {
+  token: string;
+}
+
+export interface ParticipantAuthResponseDTO extends AuthResponseDTO {
+  roomId: number;
+}
+
+export interface JwtParticipantResponseDTO {
+  uuid: string;
+  roomId: number;
+}
+
+export interface JwtUserResponseDTO {
+  id: number;
+  userId: string;
+}
+
+export type JwtResponseDTO = JwtParticipantResponseDTO | JwtUserResponseDTO;
+
+export interface VerifyRequestDTO {
+  token: string;
+}
+
+export interface VerifyResponseDTO {
+  role: Role;
+  participantId: UUID | null;
+  roomId: number | null;
+  userId: number | null;
+}
