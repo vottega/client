4d033c4cb7e1ac676428c66b5a802163d9d991d8
Kihyun Ryu <khryu0610@gmail.com>
2025-07-05 14:38:46 +0900

    fix: vite load env

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_SERVER_HOST || "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_SERVER_HOST || "http://localhost:9000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: true,
+    },
+  };
 });
