94bac598b364a61a175001867ba835246194be9c
Kihyun Ryu <khryu0610@gmail.com>
2025-07-05 12:22:21 +0900

    :fire: liveblocks 관련 코드 삭제

--- app/rooms/[id]/page.tsx
+++ app/rooms/[id]/page.tsx
@@ -2,12 +2,8 @@
 
 import { AppSidebar, VoteForm } from "@/app/rooms/[id]/AppSidebar";
 import { OnlineOffline } from "@/app/rooms/[id]/OnlineOffline";
-import { Room } from "@/app/rooms/[id]/Room";
 import { VoteList } from "@/app/rooms/[id]/VoteList";
 import { BreadcrumbHeader } from "@/components/BreadcrumbHeader";
-import { Avatars } from "@/components/liveblocks/Avatars";
-import { Editor } from "@/components/liveblocks/Editor";
-import { Status } from "@/components/liveblocks/Status";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
@@ -147,15 +143,6 @@ export default function Rooms({ params: { id: roomId } }: { params: { id: string
         {/* contents */}
         <Main>
           <div className="flex w-full flex-col gap-4 md:gap-8 flex-grow">
-            {/* <Card className="grow-[2] flex flex-col max-w-full">
-              <CardHeader>
-                <CardTitle>속기</CardTitle>
-                <CardDescription>회의 속기가 이루어지는 공간이에요.</CardDescription>
-              </CardHeader>
-              <CardContent className="flex-grow h-0">
-                <Shorthand pageId={pageId} />
-              </CardContent>
-            </Card> */}
             <Card className="flex-grow">
               <CardHeader className="flex-row items-center space-y-0">
                 <CardTitle>투표 정보</CardTitle>
@@ -192,22 +179,5 @@ export default function Rooms({ params: { id: roomId } }: { params: { id: string
 
       <AppSidebar side="right" roomId={roomId} />
     </SidebarProvider>
-  );
-}
-
-function Shorthand({ pageId }: { pageId: string }) {
-  return (
-    <Room pageId={pageId}>
-      {/* Sticky header */}
-      <div className="sticky top-0 left-0 right-0 h-[60px] flex items-center justify-between px-4 z-20">
-        <div className="absolute top-3 left-3">
-          <Status />
-        </div>
-        <div />
-        <Avatars />
-      </div>
-
-      <Editor />
-    </Room>
   );
 }
